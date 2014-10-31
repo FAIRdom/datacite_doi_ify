f2072e54dc9991ff61dc755a44725b589658731e
quyen <thucquyendn@gmail.com>
2014-10-31 14:19:43 +0100

    pending the tests

--- spec/datacite_doi_ify_spec.rb
+++ spec/datacite_doi_ify_spec.rb
@@ -3,48 +3,34 @@ require 'spec_helper'
 describe DataciteDoiIfy do
 
   describe 'resolve a DOI' do
-    it 'returns a url associated with a given DOI' do
-      pending
-    end
+    it 'returns a url associated with a given DOI'
   end
 
   describe 'mint a DOI' do
     context 'DOI does not exist' do
-      it 'mint a new DOI' do
-        pending
-      end
+      it 'mint a new DOI'
     end
 
     context 'DOI exists' do
-      it 'updates the URL' do
-        pending
-      end
+      it 'updates the URL'
     end
   end
 
   describe 'retrieve metadata for a DOI' do
-    it 'returns metadata associated with a given DOI' do
-      pending
-    end
+    it 'returns metadata associated with a given DOI'
   end
 
   describe 'update metadata for a DOI' do
-    it 'creates new version of metadata' do
-      pending
-    end
+    it 'creates new version of metadata'
   end
 
   describe 'inactivate a DOI' do
-    it 'marks a dataset as inactive' do
-      pending
-    end
+    it 'marks a dataset as inactive'
   end
 
   describe 'activate a DOI' do
     context 'after a DOI being inactivated'
-      it 'post new metadata for a given DOI' do
-        pending
-      end
+      it 'post new metadata for a given DOI'
   end
 
   #TODO: Media API
