c426b2ef266cdbfcdd52541329d82d9f077aa3f1
quyen <thucquyendn@gmail.com>
2014-10-31 13:49:18 +0100

    skeleton for the tests

--- spec/datacite_doi_ify_spec.rb
+++ spec/datacite_doi_ify_spec.rb
@@ -2,4 +2,42 @@ require 'spec_helper'
 
 describe DataciteDoiIfy do
 
+  describe 'resolve a DOI' do
+    it 'returns a url associated with a given DOI' do
+      pending
+    end
+  end
+
+  describe 'mint a DOI' do
+    context 'DOI does not exist' do
+      it 'mint a new DOI' do
+        pending
+      end
+    end
+
+    context 'DOI exists' do
+      it 'updates the URL' do
+        pending
+      end
+    end
+  end
+
+  describe 'retrieve metadata for a DOI' do
+    it 'returns metadata associated with a given DOI' do
+      pending
+    end
+  end
+
+  describe 'update metadata for a DOI' do
+    it 'creates new version of metadata' do
+      pending
+    end
+  end
+
+  describe 'inactivate a DOI' do
+    it 'marks a dataset as inactive' do
+      pending
+    end
+  end
+
 end
