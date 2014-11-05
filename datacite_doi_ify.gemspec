78febd949c2f76944c44c29743b4513d919de117
quyen <thucquyendn@gmail.com>
2014-11-05 12:18:27 +0100

    using vcr with webmock

--- datacite_doi_ify.gemspec
+++ datacite_doi_ify.gemspec
@@ -30,4 +30,5 @@ Gem::Specification.new do |s|
   s.add_development_dependency "pry-nav"
   s.add_development_dependency "coveralls"
   s.add_development_dependency "webmock"
+  s.add_development_dependency "vcr"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,19 @@
 require 'coveralls'
 Coveralls.wear!
 
+require 'vcr'
 require 'pry'
 require 'datacite_doi_ify'
 require 'webmock/rspec'
+
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/cassettes'
+  c.hook_into :webmock
+  c.default_cassette_options = { :record => :none }
+  c.configure_rspec_metadata!
+end
+
+RSpec.configure do |c|
+
+end
